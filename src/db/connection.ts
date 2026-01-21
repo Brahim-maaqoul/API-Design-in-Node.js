4c2a0f0e0c1bfa2602e0d5e738f54d8f4178c813
brahim <wacakoule@gmail.com>
2026-01-21 23:59:35 +0100

    DB seeding

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -3,7 +3,6 @@ import { Pool } from "pg";
 import * as schema from "./schema.ts"
 import { env, isProd } from "../../env.ts"
 import { remember } from "@epic-web/remember";
-import { C } from "vitest/dist/chunks/reporters.d.BFLkQcL6.js";
 
 const createPool = () => {
     return new Pool({
@@ -20,4 +19,5 @@ if (isProd()){
 }
 
 export const db = drizzle({ client, schema })
+
 export default db
